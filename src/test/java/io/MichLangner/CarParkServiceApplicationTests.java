222b62d03a6d7649d83373d9b5126b878db220df
Michal <michal.langner@gmail.com>
2022-12-04 14:38:02 +0100

    Add initial commit.

---
+++ src/test/java/io/MichLangner/CarParkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.MichLangner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarParkServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
